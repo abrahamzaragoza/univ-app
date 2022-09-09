2cca9263d54bf7d013e66eda496ba58604d4769f
abrahamzaragoza <abraham.zaragoza@enroutesystems.com>
2022-09-09 12:55:27 -0500

    Add Password field to Student, Add Password to Form, Add Password to Students Controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_08_213434) do
+ActiveRecord::Schema.define(version: 2022_09_09_165751) do
 
   create_table "courses", force: :cascade do |t|
     t.string "short_name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2022_09_08_213434) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
