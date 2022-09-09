fe856a755e0e12f7dfcfd5209928f77ab5a96070
abrahamzaragoza <abraham.zaragoza@enroutesystems.com>
2022-09-09 14:43:47 -0500

    Add StudentCourse model with M2M association, Add enrollment funcionality in backend & frontend

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,7 @@ class Course < ApplicationRecord
   validates :short_name, presence: true, length: { minimum: 5 }
   validates :name, presence: true, length: { minimum: 5, maximum: 35 }
   validates :description, presence: true, length: { minimum: 15, maximum: 300 }
+
+  has_many :student_courses
+  has_many :students, through: :student_courses
 end
